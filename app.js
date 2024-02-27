ca6f3c3377badaa6be434bd48f24b660e35c9d85
Hameer Ahmed <78217588+Hameer-Ahmed@users.noreply.github.com>
2024-02-27 20:25:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+//1.
+// alert("Hello World");
+
+//2.
+// alert("Error! Please Enter the Valid Password");
+
+//3.
+// alert("Welcome to JS Land....\nHappy Coding");
+
+//4.
+// alert("Welcome to JS Land...");
+
+//5.
+// alert("Hello... I can run JS through my web browers console");
